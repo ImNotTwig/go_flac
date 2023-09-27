de8c85c2b927d947f20290007e71932043374f16
ImNotTwig <ImNotTwig@gmail.com>
2023-09-27 02:03:00 -0500

    added MetadataBlockDataType enum & made the MetadataBlockHeader type

--- main.go
+++ main.go
@@ -7,10 +7,30 @@ import (
 )
 
 type Stream struct {
-    Signature []byte    // First 4 bytes
-    MetaDataBlocks []byte
-    AudioFrames []byte
+	Signature      []byte // First 4 bytes
+	MetaDataBlocks []byte
+	AudioFrames    []byte
 }
+
+type MetadataBlockHeader struct {
+	IsLast   bool
+	Type     MetadataBlockDataType
+	DataSize uint
+}
+
+// METADATA_BLOCK_DATA types
+
+type MetadataBlockDataType uint8
+
+const (
+	METADATA_BLOCK_STREAMINFO MetadataBlockDataType = iota
+	METADATA_BLOCK_PADDING
+	METADATA_BLOCK_APPLICATION
+	METADATA_BLOCK_SEEKTABLE
+	METADATA_BLOCK_VORBIS_COMMENT
+	METADATA_BLOCK_CUESHEET
+	METADATA_BLOCK_PICTURE
+)
 
 // this array of bytes is what is in the first 4 bytes of a flac file
 var FLAC_LABEL = []byte{'f', 'L', 'a', 'C'}
@@ -20,20 +40,29 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-    // if the beginning of the file does not have the "fLaC" array of bytes,
-    // it is not a flac
-    if string(file[:4]) != string(FLAC_LABEL) {
-        panic("File is not a .flac")
-    }
-    fmt.Println(file[4:8])
-    // MetaData Block Header
-    fmt.Println(uint(file[4] & 1))
-    fmt.Println(uint(file[4] & 0x7F))
-    fmt.Println(file[5:8])
-    // Convert bytes to an integer, for the byte length of the metadata block
-    var byteSize uint32
-    for i := 0; i < 3; i++ {
-        byteSize |= uint32(file[5:8][i]) << (8 * uint(i))
-    }
 
+	// if the beginning of the file does not have the "fLaC" array of bytes,
+	// it is not a flac
+	if string(file[:4]) != string(FLAC_LABEL) {
+		panic("File is not a .flac")
+	}
+	fmt.Println(ReadBlockHeader(file[4:8]))
+}
+
+func ReadBlockHeader(blockHeader []byte) MetadataBlockHeader {
+	isLast := uint(blockHeader[0] & 1)
+	return MetadataBlockHeader {
+		Type:     MetadataBlockDataType(blockHeader[0] & 0x7F),
+		IsLast:   isLast != 0,
+		DataSize: GetSizeOfBlockData(blockHeader[0:4]),
+	}
+}
+
+func GetSizeOfBlockData(blockHeader []byte) uint {
+	// Convert bytes to an integer, for the byte length of the metadata block
+	var byteSize uint
+	for i := 0; i < 3; i++ {
+		byteSize |= uint(blockHeader[0:4][i]) << (8 * uint(i))
+	}
+	return byteSize
 }
